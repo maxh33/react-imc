17dbbc997e02b7710d5a928780a956ae55feaf37
maxh33 <contact@maxhaider.dev>
2024-11-13 14:04:21 -0300

    feat: result

---
+++ src/components/Result/Result.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Result = ({ imc }) => {
+    return (
+        <div>
+            <h2>Your IMC: {imc}</h2>
+        </div>
+    );
+};
+
+export default Result;
