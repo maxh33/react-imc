33a445987895c0ba59e6216c95c12f813d738960
maxh33 <contact@maxhaider.dev>
2024-11-13 15:21:21 -0300

    feat: result message

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './Result.module.css';
+import ResultMessage from '../ResultMessage/ResultMessage';
 
 
 const Result = ({ imc }) => {
     return (
         <div className={styles.result}>
             <h2>Your IMC: {imc}</h2>
+            <ResultMessage imc={imc} />
         </div>
     );
 };
